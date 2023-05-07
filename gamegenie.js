0a74f3200a4905af7834ed12c34aaae040889571
Tolstoj <martin.werner.jakob@gmail.com>
2023-05-07 21:23:57 +0200

    first commit

---
+++ gamegenie.js
@@ -0,0 +1,121 @@
+function handleInput() {
+      
+    // This whole function could be optimized... a lot! It feels (and is) so inefficient 
+
+    var input = document.getElementById("hexInput").value.replace(/[-x\s]/gi, "").toUpperCase();
+    
+    // Only allow 6 or 9 digit hexadecimal values
+    if (/^[0-9A-F]{6}$/.test(input) || /^[0-9A-F]{9}$/.test(input)) {
+      var A = parseInt(input.charAt(0), 16).toString(16);
+      var B = parseInt(input.charAt(1), 16).toString(16);
+      var C = parseInt(input.charAt(2), 16).toString(16);
+      var D = parseInt(input.charAt(3), 16).toString(16);
+      var E = parseInt(input.charAt(4), 16).toString(16);
+      var F = (0xF - parseInt(input.charAt(5), 16)).toString(16);
+      var G = parseInt(input.charAt(6), 16).toString(2).padStart(4, '0');
+      var G2 = "";
+      var H = "";
+      var HInv = "";
+      var GInv = "";
+
+      if (input.length === 9) {
+        G2 = parseInt(input.charAt(7), 16).toString(2).padStart(4, '0');
+        H = parseInt(input.charAt(8), 16).toString(2).padStart(4, '0');
+      }
+
+      var Gl = G.length;
+      var G2l = G2.length;
+      var Hl = H.length;
+
+      for (i = 0; i < Gl; i++) {
+        var add = 1;
+        if (G.charAt(i) === "1") add = 0;
+        GInv += add;
+      }
+
+      for (i = 0; i < Hl; i++) {
+        var add = 1;
+        if (H.charAt(i) === "1") add = 0;
+        HInv += add;
+      }
+
+      var isSame = true;
+      var i = 0;
+      if (Gl === G2l) {
+        while (isSame && i < Gl) {
+          if (i === 0) {
+            if (G.charAt(i) === G2.charAt(i)) {
+              isSame = false;
+            }
+          } else {
+            if (G.charAt(i) !== G2.charAt(i)) {
+              isSame = false;
+            }
+          }
+          i++;
+        }
+      }
+
+      if (G2 === "" && H === "") {
+        isSame = true;
+      }
+
+      if (isSame) {
+        var hexAddress = (F + C + D + E).toUpperCase();
+        var oldValue = HInv.charAt(2) + H.charAt(3) + GInv.charAt(0) + GInv.charAt(1) + GInv.charAt(2) + G.charAt(3) + HInv.charAt(0) + H.charAt(1);
+        oldValue = parseInt(oldValue, 2).toString(16).padStart(2, '0').toUpperCase();
+        var newValue = (A + B).toUpperCase();
+
+        if (G2 === "" && H === "") {
+          oldValue = "-";
+        }
+
+        document.getElementById("romAddr").value = hexAddress;
+        document.getElementById("oldVal").value = oldValue;
+        document.getElementById("newVal").value = newValue;
+
+      } else {
+        alert("The Game Genie code failed its checksum test! Check the spelling of your code and try again.");
+      }
+    } else {
+      alert("This is not a valid Game Genie code!");
+    }
+  }
+
+  function applyCode(){
+    address = document.getElementById("romAddr").value.trim();
+    oldVal = document.getElementById("oldVal").value.trim();
+    newVal = document.getElementById("newVal").value.trim();
+    valueInRomAddress = document.getElementById(address).textContent;
+    
+    // scroll to the address
+    scrollToAddress(address);
+    
+    //setTimeout(function() { // <-- this makes sure that the scrolling is done before the rest
+        // only change the value if the old value was the one from the GG code
+        if(oldVal == "-" || oldVal == valueInRomAddress){
+            //exchange the value in the cell
+            document.getElementById(address).textContent = newVal;
+            document.getElementById(address).classList.add("edited");
+            if(log != ""){
+
+            }
+            alert("the value has been altered successfully!");
+            const date = new Date();
+            const options = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+            const formattedTime = date.toLocaleTimeString(undefined, options);
+
+            addToLog("* GG code added: Address $" + address + " | " + oldVal + " > " + newVal + " (" + formattedTime + ")");
+        }else{
+            alert("According to the Game Genie code you provided, the ROM Address $" + address + " should contain the value 0x" + oldVal + ". This was not the case and nothing has been changed!\l Make sure the Game Genie code is correct and belongs to this game.");
+        }
+
+        // clear the fields
+        document.getElementById("romAddr").value = "";
+        document.getElementById("oldVal").value = "";
+        document.getElementById("newVal").value = "";
+        document.getElementById("hexInput").value = "";
+        
+
+    //}, 800);
+  }
---
+++ hexeditor.js
@@ -0,0 +1,238 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+// ROM HEX Editor and Game Genie code patcher
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+// 
+// Todo:
+// -----
+// * Toast messages instead of the alerts
+// * Only show Address, Old and New, if a GG code has been entered and is correct
+// * Correct the global checksum (https://gbdev.io/pandocs/The_Cartridge_Header.html)
+// * make sure the classes editing and edited are assigned correctly
+// * only show the save option, when at least one modification has been made
+// * make the appearance nicer - CSS styles
+// 
+// Tasks for the future:
+// --------------------
+// * get the header information
+// * maybe make some tweaks easier
+// * add a ROM map (https://datacrystal.romhacking.net/wiki/Tetris_(Game_Boy):ROM_map)
+// * add a RAM map (https://datacrystal.romhacking.net/wiki/Tetris_(Game_Boy):RAM_map)
+// * check out how difficult it is to work with ROMs that require ROM bank switching
+//    * maybe the banks are fix - then it shouldn't be a problem
+// * identify the tiles and make them editable
+// * find the OP-codes and also show these in assembly style
+// * identify tables
+// 
+// Tolstoj & ChatGPT 2023
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+function validateFile(event) {
+    var file = event.target.files[0];
+    
+    // Check if a file is selected
+    if (!file) {
+      alert('Please select a file.');
+      return false;
+    }
+    
+    // Check the file extension
+    splitFileName = file.name.split('.');
+
+    var fileExtension = splitFileName.pop().toLowerCase();
+    if (fileExtension !== 'gb') {
+      alert('Only .gb files are allowed.');
+      return false;
+    }
+
+    newFileName = "";
+    for(i=0; i<splitFileName.length; i++){
+        newFileName += splitFileName[i];
+    }
+    newFileName += "-modified";
+    document.getElementById("patchRomName").value = newFileName;
+    
+    // Check the file size
+    var fileSize = file.size / 1024; // in KB
+    if (fileSize > 32) {
+      alert('File size should be less than or equal to 32 KB.');
+      return false;
+    }
+    
+    // Read the file data
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      var fileData = event.target.result;
+      var hexData = convertToHex(fileData);
+      
+      // Display or process the hex data
+      displayHexData(hexData);
+      
+      // show the hidden elements
+      document.getElementById('wrapper').style.display = 'block';
+
+    };
+
+    reader.readAsArrayBuffer(file);
+
+    return true;
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var fileInput = document.getElementById('fileInput');
+    fileInput.addEventListener('change', validateFile);
+  });
+
+  function createFileFromHexData() {
+    const table = document.getElementById('hexViewer');
+    const rows = table.rows;
+
+    // Create a Uint8Array to hold the file data
+    const fileSize = (rows.length - 1) * 16;
+    const fileData = new Uint8Array(fileSize);
+
+    for (let i = 1; i < rows.length; i++) {
+      const cells = rows[i].cells;
+
+      for (let j = 1; j < cells.length; j++) {
+        const cell = cells[j];
+        const hexValue = cell.textContent || '00';
+        const byteValue = parseInt(hexValue, 16);
+        fileData[(i - 1) * 16 + (j - 1)] = byteValue;
+      }
+    }
+
+    // Create a Blob from the Uint8Array
+    const blob = new Blob([fileData]);
+
+    // Create a download link and trigger the download
+    newFileName = 'modified_ROM.gb';
+    fileNameFromInput = document.getElementById("patchRomName").value + ".gb";
+    if(fileNameFromInput != "") newFileName = fileNameFromInput;
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = newFileName;
+    link.click();
+  }
+
+  function convertToHex(fileData) {
+    const view = new DataView(fileData);
+    const hexValues = [];
+
+    for (let i = 0; i < view.byteLength; i++) {
+      const hex = view.getUint8(i).toString(16).toUpperCase().padStart(2, '0');
+      hexValues.push(hex);
+    }
+
+    return hexValues;
+  }
+
+  function displayHexData(hexData) {
+    const table = document.getElementById('hexViewer');
+    table.innerHTML = '';
+
+    // Create the header row
+    const headerRow = table.insertRow();
+    headerRow.id = 'headerRow';
+    const addressHeader = document.createElement('th');
+    addressHeader.textContent = 'Address';
+    headerRow.appendChild(addressHeader);
+
+    for (let i = 0; i < 16; i++) {
+      const hexDigit = i.toString(16).toUpperCase();
+      const headerCell = document.createElement('th');
+      headerCell.textContent = hexDigit;
+      headerRow.appendChild(headerCell);
+    }
+
+    for (let i = 0; i < hexData.length; i += 16) {
+      const row = table.insertRow(); // Add this line to create a new row
+      const addressCell = row.insertCell();
+      const hexValueCells = [];
+
+      const address = i.toString(16).toUpperCase().padStart(4, '0').slice(0,3) + "_";
+      const addressID = i.toString(16).toUpperCase().padStart(4, '0');//address.slice(0, 3) + (parseInt(address.slice(-1), 16) - 1).toString(16).toUpperCase();
+      addressCell.innerHTML = `<a href="#${addressID}"></a>${address}`;
+      addressCell.id = address;
+
+      for (let j = 0; j < 16; j++) {
+        const hexValue = hexData[i + j] || '';
+        const hexValueCell = row.insertCell();
+        hexValueCell.className = 'hexValueCell';
+        hexValueCell.textContent = hexValue;
+        hexValueCell.contentEditable = true;
+        const cellID = addressID.slice(0, 3) + j.toString(16).toUpperCase();
+        hexValueCell.id = cellID;
+        hexValueCells.push(hexValueCell);
+      }
+
+      hexValueCells.forEach(cell => {
+        cell.addEventListener('input', function(event) {
+          const cell = event.target;
+          cell.classList.add('editing');
+          if (!cell.hasAttribute('data-previous-value')) {
+            cell.setAttribute('data-previous-value', cell.textContent);
+          }
+        });
+
+        cell.addEventListener('blur', function(event) {
+          const cell = event.target;
+          const value = cell.textContent;
+
+          // Validate input
+          if (!/^[0-9A-Fa-f]{0,2}$/.test(value)) {
+            cell.textContent = value.slice(0, 2);
+            return;
+          }
+
+          // Remove any leading zeros
+          cell.textContent = value.toUpperCase().padStart(2, '0').slice(-2);
+
+          // Check if the value has changed
+          const previousValue = cell.getAttribute('data-previous-value');
+          cell.removeAttribute('data-previous-value');
+          cell.classList.remove('editing');
+
+          if (previousValue !== cell.textContent) {
+            cell.classList.add('edited');
+          } else {
+            cell.classList.remove('edited');
+          }
+        });
+      });
+    }
+  }
+
+  function addToLog(logText){
+    // Get a reference to the div element
+    const logDiv = document.getElementById("log");
+
+    // Create a new text node with the desired content
+    const newLineText = document.createTextNode(logText);
+
+    // Create a new <br> element for the line break
+    const lineBreak = document.createElement("br");
+
+    // Append the new text node and line break to the div
+    logDiv.appendChild(newLineText);
+    logDiv.appendChild(lineBreak);
+  }
+
+  function scrollToAddress(address) {
+    address = (parseInt("0x" + address, 16) - 16).toString(16).toUpperCase();
+    while(address.length < 4){
+        address = "0" + address;
+    }
+    address = address.slice(0, -1) + "0";
+    const anchorElement = document.getElementById(address);
+    anchorElement.scrollIntoView({ behavior: 'smooth' });
+  }
+  
+
+  function searchAndSelectCell() {
+    const searchInput = document.getElementById('searchInput');
+    const address = searchInput.value.trim();
+    scrollToAddress(address);
+  }
