087d7a545dee5dc8f647bc66369b45f0da478f46
Tolstoj <martin.werner.jakob@gmail.com>
2023-05-23 22:21:07 +0200

    now it's super slow

--- hexeditor.js
+++ hexeditor.js
@@ -794,3 +794,24 @@ function getBGMap(id) {
   document.getElementById("BGMapStartAddress").value = id;
   
 }
+
+//------------------------------------------------------------------------------------------
+
+// tab group
+function openTab(event, tabName) {
+  var i, tabContent, tab;
+
+  tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  tab = document.getElementsByClassName("tab");
+  for (i = 0; i < tab.length; i++) {
+    tab[i].className = tab[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+
+  event.currentTarget.className += " active";
+}
