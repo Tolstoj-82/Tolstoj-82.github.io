85f37b84168bffd94fc6904b5e1b40baea7ebf9a
Tolstoj <martin.werner.jakob@gmail.com>
2024-09-13 17:31:19 +0200

    what?

--- Live/refactored/JS/OCR.js
+++ Live/refactored/JS/OCR.js
@@ -142,11 +142,11 @@ function populatePlayfield(array) {
     
     array.forEach((value, index) => {
         if (gridCells[index]) {
-            if (value != "0") {
+            //if (value != "0") {
                 gridCells[index].style.backgroundImage = `url(images/tiles/${scheme}/${value}.png)`;
-            } else {
-                gridCells[index].style.backgroundImage = '';
-            }
+            //} else {
+            //    gridCells[index].style.backgroundImage = '';
+            //}
         }
     });
 }
