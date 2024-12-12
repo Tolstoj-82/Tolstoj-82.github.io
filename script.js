26d48fcef190f63aa28c8102dfbf0dc4a320d68b
Tolstoj <martin.werner.jakob@gmail.com>
2024-12-12 21:32:58 +0100

    books kinda added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('scroll', function () {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-      header.classList.add('small');
-    } else {
-      header.classList.remove('small');
-    }
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {
+    header.classList.add('small');
+  } else {
+    header.classList.remove('small');
+  }
 });
