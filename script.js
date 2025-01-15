50402c46b58a70f2c891d56f7e4adb44662ff4fd
Tolstoj <martin.werner.jakob@gmail.com>
2025-01-15 23:24:49 +0100

    updates

--- script.js
+++ script.js
@@ -1,44 +1,21 @@
-/*document.addEventListener('scroll', function () {
-  const header = document.querySelector('header');
-  if (window.scrollY > 50) {
-    header.classList.add('small');
-  } else {
-    header.classList.remove('small');
-  }
-});*/
-/*
 document.addEventListener('DOMContentLoaded', () => {
   const images = document.querySelectorAll('.clickable');
   const modal = document.getElementById('imageModal');
   const modalImage = document.getElementById('modalImage');
   const closeModal = modal.querySelector('.close');
+  const accordions = document.querySelectorAll('.accordion');
 
-  // Attach click event to all clickable images
-  images.forEach(image => {
-    image.addEventListener('click', () => {
-      modalImage.src = image.src; // Set the modal image to the clicked image
-      modal.style.display = 'flex'; // Show the modal
+  // Accordions
+  accordions.forEach((accordion) => {
+    accordion.addEventListener('click', () => {
+        const menu = accordion.nextElementSibling;
+
+        if (menu && menu.classList.contains('menu')) {
+            menu.classList.toggle('active');
+            accordion.classList.toggle('active');
+        }
     });
   });
-
-  // Close the modal when the close button is clicked
-  closeModal.addEventListener('click', () => {
-    modal.style.display = 'none'; // Hide the modal
-  });
-
-  // Close the modal when clicking outside the modal-content
-  modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
-      modal.style.display = 'none';
-    }
-  });
-});
-*/
-document.addEventListener('DOMContentLoaded', () => {
-  const images = document.querySelectorAll('.clickable');
-  const modal = document.getElementById('imageModal');
-  const modalImage = document.getElementById('modalImage');
-  const closeModal = modal.querySelector('.close');
 
   // Attach click event to all clickable images
   images.forEach(image => {
